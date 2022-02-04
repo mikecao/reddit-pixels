eefd32cf35b1975b255a718e5ae511b15615306d
Mike Cao <mike@mikecao.com>
2022-02-04 00:14:49 -0800

    Remove unused API endpoints.

--- pages/api/test.js
+++
@@ -1,8 +0,0 @@
-import { encrypt, decrypt } from 'lib/crypto';
-
-export default async (req, res) => {
-  const enc = encrypt('hello');
-  const dec = decrypt(enc);
-
-  res.status(200).json({ enc, dec });
-};
