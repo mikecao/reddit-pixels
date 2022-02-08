ae8a02375b5d8a1746e5bba1689e5088f90dc4ed
Mike Cao <mike@mikecao.com>
2022-02-08 02:35:12 -0800

    Fix issue with some thumbnails.

--- components/Links.js
+++ components/Links.js
@@ -7,16 +7,17 @@ import Image from 'assets/image.svg';
 import Plus from 'assets/plus.svg';
 import styles from './Links.module.css';
 import classNames from 'classnames';
+import { getExtension } from '../lib/utils';
 
 export default function Links({ item }) {
   const [show, setShow] = useState(true);
-  const { permalink, subreddit, author, url } = item;
+  const { id, permalink, subreddit, author, src } = item;
 
   const links = [
     [Message, `https://www.reddit.com/${permalink}`, 'comments'],
     [Reddit, `https://www.reddit.com/r/${subreddit}`, 'subreddit'],
     [User, `https://www.reddit.com/user/${author}`, 'poster'],
-    [Image, url, 'media'],
+    [Image, src, 'media'],
   ];
 
   function handleClick(e) {
@@ -34,7 +35,14 @@ export default function Links({ item }) {
     <div className={styles.links}>
       {show &&
         links.map(([icon, url, title], i) => (
-          <a key={i} href={url} target="_blank" rel="noreferrer" onClick={handleClick}>
+          <a
+            key={i}
+            href={url}
+            target="_blank"
+            rel="noreferrer"
+            onClick={handleClick}
+            download={`${id}.${getExtension(url)}`}
+          >
             <Icon icon={icon} />
             <div className={styles.title}>{title}</div>
           </a>
